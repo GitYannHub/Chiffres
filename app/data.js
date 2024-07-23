4f57e3d2c36966c877d561abb31169843cae25f9
Yann local <y.annimmo.13@gmail.com>
2024-07-23 20:08:28 +0200

    ok

--- app/data.js
+++ app/data.js
@@ -3,64 +3,64 @@ const Constants= {
     nbData:10,
     data:{
       1: {
-          url:require("../assets/blanc.png"),
-          nom:"Blanc",
-          emotion:"Pureté, innocence",
-          emotion2:"Simplicité, paix, propreté"
+          url:require("../assets/projet/1.png"),
+          nom:"Un",
+          emotion:"Le nombre d'unités dans un système de numérotation de base, représentant l'individualité et la singularité.",
+          emotion2:"En informatique, le chiffre 1 est utilisé dans le système binaire, qui est la base du fonctionnement des ordinateurs. Le 1 veut dire 'vrai'."
           },
       2: {
-          url:require("../assets/bleu.png"),
-          nom:"Bleu",
-          emotion:"Calme, sérénité",
-          emotion2:"Tristesse, confiance, loyauté"
+          url:require("../assets/projet/2.png"),
+          nom:"Deux",
+          emotion:"Les deux pôles terrestres (Nord et Sud).",
+          emotion2:"En Chine, le chiffre deux est souvent associé à la dualité et à l'équilibre, comme le yin et le yang."
         },
       3: {
-          url:require("../assets/gris.png"),
-          nom:"Gris",
-          emotion:"Neutralité, équilibre",
-          emotion2:"Formalité, réserve, tristesse"
+          url:require("../assets/projet/3.png"),
+          nom:"Trois",
+          emotion:" Les trois dimensions de l'espace (longueur, largeur, hauteur).",
+          emotion2:"Dans les contes de fées, les épreuves ou souhaits sont souvent donnés par groupes de trois, symbolisant un cycle complet ou une progression."
         },
       4: {
-          url:require("../assets/jaune.png"),
-          nom:"Jaune",
-          emotion:"Joie, bonheur",
-          emotion2:"Énergie, optimisme, attention"
+          url:require("../assets/projet/4.png"),
+          nom:"Quatre",
+          emotion:"Les quatre points cardinaux (nord, sud, est, ouest).",
+          emotion2:"Dans certaines cultures asiatiques, le chiffre quatre est évité car il se prononce comme le mot 'mort'."
         },
       5: {
-          url:require("../assets/marron.png"),
-          nom:"Marron",
-          emotion:"Terre, stabilité",
-          emotion2:"Confort, simplicité, sécurité"
+          url:require("../assets/projet/5.png"),
+          nom:"Cing",
+          emotion:"Les cinq sens humains (vue, ouïe, odorat, goût, toucher).",
+          emotion2:"Le pentagramme, une étoile à cinq branches, est utilisé dans de nombreuses cultures pour représenter la protection et l'équilibre."
         },
       6: {
-          url:require("../assets/orange.png"),
-          nom:"Orange",
-          emotion:"Enthousiasme, chaleur",
-          emotion2:"Créativité, excitation, encouragement"
+          url:require("../assets/projet/6.png"),
+          nom:"Six",
+          emotion:"Les six faces d'un dé.",
+          emotion2:"En géométrie, un hexagone est une figure à six côtés, souvent visible dans la structure des nids d'abeilles."
         },
       7: {
-          url:require("../assets/rose.png"),
-          nom:"Rose",
-          emotion:"Douceur, amour",
-          emotion2:"Compassion, innocence, tendresse"
+          url:require("../assets/projet/7.png"),
+          nom:"Sept",
+          emotion:"Les sept jours de la semaine.",
+          emotion2:"Les sept merveilles du monde antique, représentant les constructions humaines les plus impressionnantes de l'époque."
         },
       8: {
-          url:require("../assets/rouge.png"),
-          nom:"Rouge",
-          emotion:"Passion, énergie",
-          emotion2:"Amour, colère, urgence"
+          url:require("../assets/projet/8.png"),
+          nom:"Huit",
+          emotion:"Les huit notes d'une gamme musicale",
+          emotion2:"Le chiffre huit est considéré comme très chanceux en Chine, car il se prononce comme le mot 'prospérité'"
         },
       9: {
-          url:require("../assets/vert.png"),
-          nom:"Vert",
-          emotion:"Nature, tranquillité",
-          emotion2:"Espoir, renouveau, croissance"
+          url:require("../assets/projet/9.png"),
+          nom:"Neuf",
+          emotion:"Les neuf planètes du système solaire (en comptant pluton)",
+          emotion2:"En mathématiques, le chiffre neuf a la propriété intéressante que la somme des chiffres de ses multiples est toujours un multiple de neuf."
         },
       10: {
-          url:require("../assets/violet.png"),
-          nom:"Violet",
-          emotion:"Mystère, spiritualité",
-          emotion2:"Royauté, luxe, créativité"
+          url:require("../assets/projet/0.png"),
+          nom:"Zéro",
+          emotion:"Le zéro degrè K soit La température absolue dans certaines parties de l'univers",
+          emotion2:"Le chiffre 0 a été inventé par les mathématiciens indiens autour du 5ème siècle."
         },
     }
     
