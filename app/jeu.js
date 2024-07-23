a17d1d03a58c1fb7b2b5b14b8473f685d717df65
Yann local <y.annimmo.13@gmail.com>
2024-07-23 21:17:08 +0200

    ok

--- app/jeu.js
+++ app/jeu.js
@@ -25,76 +25,75 @@ Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
 async function playSoundObjet(index) {
   console.log('Loading Sound');
 
-if (index==1){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/blanc.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==2){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/bleu.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==3){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/gris.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==4){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/jaune.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==5){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/marron.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==6){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/orange.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==7){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/rose.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==8){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/rouge.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==9){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/vert.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-
-if (index==10){
-  const { sound } = await Audio.Sound.createAsync( require('../assets/sons/violet.mp3'));
-  setSound(sound);
-  console.log('Playing Sound');
-  await sound.playAsync();
-}
-      
+  if (index==1){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/1.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==2){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/2.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==3){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/3.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==4){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/4.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==5){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/5.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==6){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/6.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==7){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/7.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==8){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/8.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==9){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/9.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  
+  if (index==10){
+    const { sound } = await Audio.Sound.createAsync( require('../assets/sons/0.mp3'));
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }     
 }
 
 async function playSoundBonneReponse() {
